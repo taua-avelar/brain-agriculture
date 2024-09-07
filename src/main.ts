bea87a52351bcfae7002c0e3041a4441fbeb71b2
taua <taua@incentivar.io>
2024-09-07 02:47:59 -0300

    feat: requests body validation and add port to env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () =>
+    console.log('Aplicação rodando na porta ' + port),
+  );
 }
 bootstrap();
