f7f9f7d5c519e6620de2a41aab61fa7e134e22f6
taua <taua@incentivar.io>
2024-09-07 02:49:50 -0300

    clear app resource and add sqlite database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { FarmerModule } from './farmer/farmer.module';
+import { Farmer } from './farmer/farmer.entity';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'db.sqlite',
+      entities: [Farmer],
+      synchronize: true, // desabilitar em prod
+    }),
+    FarmerModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
